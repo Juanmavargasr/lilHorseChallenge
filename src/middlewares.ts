ad8379e8e76a0944222c9845839c453cd1bf2fbc
Juan Manuel Vargas <juanmavargasr@gmail.com>
2023-05-22 02:32:31 -0500

    README.md finished

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -2,27 +2,6 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { jwtSecret } from "./controllers";
 
-// const router = express.Router();
-
-// export const authenticateToken = (
-//   req: express.Request,
-//   res: express.Response,
-//   next: express.NextFunction
-// ) => {
-//   const token = req.headers["authorization"];
-//   if (!token) {
-//     return res.status(401).json({ error: "Token not provided" });
-//   }
-
-//   jwt.verify(token, jwtSecret, (err: any, user: any) => {
-//     if (err) {
-//       return res.status(403).json({ error: "Invalid token" });
-//     }
-//     (req as any).user = user;
-//     next();
-//   });
-// };
-
 export const verifyToken = (
   req: express.Request,
   res: express.Response,
